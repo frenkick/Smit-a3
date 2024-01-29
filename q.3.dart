2fa1fc2c4b1a2d1fcee89217e1201c1019663dc0
frenkick <talhaaa.ayaz777@gmail.com>
2024-01-29 15:16:21 +0500

    done

---
+++ q.3.dart
@@ -0,0 +1,13 @@
+void main() {
+  List days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  days.removeLast();
+  print(days);
+}
