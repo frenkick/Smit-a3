30766f83151aa81ec993b90f5bde85a588560e17
frenkick <talhaaa.ayaz777@gmail.com>
2024-02-06 15:55:22 +0500

    done

---
+++ Q.9.dart
@@ -0,0 +1,16 @@
+void main() {
+  List l1 = [
+    23,
+    12,
+    656,
+    7787,
+    4,
+    343,
+    2,
+    5,
+  ];
+  print("Max value");
+  l1.sort();
+  print(l1[l1.length - 1]);
+  print(l1);
+}
