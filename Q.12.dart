fc81436fe9df1e029ed690fccb5d2b69acdb6c4b
frenkick <talhaaa.ayaz777@gmail.com>
2024-02-09 16:47:45 +0500

    m

---
+++ Q.12.dart
@@ -0,0 +1,8 @@
+void main() {
+  List l1 = ["A", "b", 'C', 'd', 'E', 'f'];
+  List l2 = [l1.reversed];
+
+  print("Orignal List $l1");
+  print("==============================================");
+  print("Changed List $l2");
+}
