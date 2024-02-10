65d04c4cd4241c5e1d36d1ca11539680eae26e25
frenkick <talhaaa.ayaz777@gmail.com>
2024-02-10 21:16:24 +0500

    g

---
+++ Q.22.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map Cart = {
+    "Product": "Apple",
+    "Quantities": 6,
+  };
+
+  if (Cart['Product'] == "Apple") {
+    print("Product Found");
+  } else {
+    print("Product Not Found");
+  }
+}
